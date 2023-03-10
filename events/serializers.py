7365afd538d832fc83af010c2e3c449ca38e9be3
Pietro Acinapura <pietro.ac@gmail.com>
2023-03-10 11:13:50 +0100

    Cleaned code

--- events/serializers.py
+++ events/serializers.py
@@ -11,16 +11,7 @@ class EventSerializer(serializers.ModelSerializer):
         fields = ('name', 'description', 'start_date', 'end_date', 'created_by', 'created_on', 'modified_on')
         extra_kwargs = {
             'name': {'required': True},
+            'description': {'required': True},
             'start_date': {'required': True},
             'end_date': {'required': True}
         }
-
-    def create(self, validated_data):
-        obj = self.Meta.model.objects.create(
-            name=validated_data['name'],
-            description=validated_data['description'],
-            start_date=validated_data['start_date'],
-            end_date=validated_data['end_date'],
-            created_by=validated_data['created_by']
-        )
-        return obj
